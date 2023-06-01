432ff577d60c0a36d88a3f9b50a358889112733e
Dima <vidistudioproduction@gmail.com>
2023-06-01 18:18:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 
 import './App.css'
-import { Route, Routes, NavLink } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
       <h1>Dima</h1>
-      <NavLink to='/'>1</NavLink>
-      <NavLink to='/dima'>2</NavLink>
-      <Routes>
-        <Route path="/" element={<div>Hello</div>} />
-        <Route path='/dima' element={<div>Dima</div>} />
-      </Routes>
     </>
   )
 }
