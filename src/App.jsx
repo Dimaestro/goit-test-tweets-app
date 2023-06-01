e0b91f62fd46cb1059919676897fd6916114cfd6
Dima <vidistudioproduction@gmail.com>
2023-06-01 20:47:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Routes, Route, NavLink } from 'react-router-dom'
 function App() {
   return (
     <>
-      <NavLink to='/goit-test-tweets-app/'>Dima</NavLink>
+      <NavLink to='/goit-test-tweets-app/*'>Dima</NavLink>
       <NavLink to='/goit-test-tweets-app/vite'>Vite</NavLink>
       <Routes>
-        <Route path='/goit-test-tweets-app/' element={<div>Dima</div>} />
+        <Route path='/goit-test-tweets-app/*' element={<div>Dima</div>} />
         <Route path='/goit-test-tweets-app/vite' element={<div>Vite</div>} />
      </Routes>
     </>
