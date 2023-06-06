9f5a0159183b6acccbc3fe5b48eec6011c689912
Dima <vidistudioproduction@gmail.com>
2023-06-06 12:04:03 +0300

    scroll

--- src/page/Tweets.jsx
+++ src/page/Tweets.jsx
@@ -10,11 +10,21 @@ const TweetsPage = () => {
       <Tweets />
       <ScrollToTop
         className={css.scrollToTop}
+        color={"#fff"}
         top={300}
         width={18}
         height={18}
         smooth 
-        style={{bottom: 20, right: 20,}}
+        style={{
+          bottom: 20,
+          right: 20,
+          border: "1px solid transparent",
+          borderRadius: "50%",
+          width: "45px",
+          height: '45px',
+          backgroundColor: "hsla(240, 4%, 11%, 0.521)",
+          boxShadow: "0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)",
+        }}
       />
     </>
   )
